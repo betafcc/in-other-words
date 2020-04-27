42ae89df16243e10e70f0c70b1cbcb9879dd325c
Beta Faccion <betafcc@gmail.com>
2020-04-27 18:35:08 +0100

    implements #1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -47,29 +47,44 @@ export const App: FC = () => {
   useEffect(() => setQuery(state))
 
   return (
-    <InOtherWords
-      {...state}
-      translate={async (languages) => {
-        if (state.languages.length >= 2) {
-          dispatch({ type: 'start' })
+    <>
+      <InOtherWords
+        {...state}
+        translate={async (languages) => {
+          if (state.languages.length >= 2) {
+            dispatch({ type: 'start' })
 
-          for await (const el of Translator.create().inOtherWords(
-            state.inputValue,
-            state.languages as any
-          )) {
-            dispatch({ type: 'receive', payload: el.result })
+            for await (const el of Translator.create().inOtherWords(
+              state.inputValue,
+              state.languages as any
+            )) {
+              dispatch({ type: 'receive', payload: el.result })
+            }
+
+            dispatch({ type: 'stop' })
           }
-
-          dispatch({ type: 'stop' })
-        }
-      }}
-      focus={(index) => dispatch({ type: 'focus', payload: index })}
-      setInput={(value) => dispatch({ type: 'setInput', payload: value })}
-      setSearch={(value) => dispatch({ type: 'setSearch', payload: value })}
-      addLanguage={(code) => dispatch({ type: 'addLanguage', payload: code })}
-      editLanguage={(code) => dispatch({ type: 'editLanguage', payload: code })}
-      removeLanguage={() => dispatch({ type: 'removeLanguage' })}
-      closeSearch={() => dispatch({ type: 'closeSearch' })}
-    />
+        }}
+        focus={(index) => dispatch({ type: 'focus', payload: index })}
+        setInput={(value) => dispatch({ type: 'setInput', payload: value })}
+        setSearch={(value) => dispatch({ type: 'setSearch', payload: value })}
+        addLanguage={(code) => dispatch({ type: 'addLanguage', payload: code })}
+        editLanguage={(code) => dispatch({ type: 'editLanguage', payload: code })}
+        removeLanguage={() => dispatch({ type: 'removeLanguage' })}
+        closeSearch={() => dispatch({ type: 'closeSearch' })}
+      />
+      <a
+        style={{
+          display: 'block',
+          textAlign: 'right',
+          marginTop: '0.5em',
+          marginRight: '0.5em',
+          textDecoration: 'none',
+          color: '#333'
+        }}
+        href='http://translate.yandex.com'
+      >
+        Powered by Yandex
+      </a>
+    </>
   )
 }
