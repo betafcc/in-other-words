0dc1f4a731adf0681d3d6906f56d2b1cbb040001
Beta Faccion <betafcc@gmail.com>
2020-04-29 02:30:23 +0100

    Updates

---
+++ precache-manifest.e0933a4cd16e51be7d82c2ef878b5d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296d18ccba5d88d326aef6b9f7f72389",
+    "url": "/in-other-words/index.html"
+  },
+  {
+    "revision": "a323cd2fd0ea50402148",
+    "url": "/in-other-words/static/css/main.6925051a.chunk.css"
+  },
+  {
+    "revision": "7c66b87fffa88f4a62f4",
+    "url": "/in-other-words/static/js/2.ee591022.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/in-other-words/static/js/2.ee591022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a323cd2fd0ea50402148",
+    "url": "/in-other-words/static/js/main.4c265e6c.chunk.js"
+  },
+  {
+    "revision": "5c9508dbcefe0391c0cf",
+    "url": "/in-other-words/static/js/runtime-main.d436f349.js"
+  }
+]);
